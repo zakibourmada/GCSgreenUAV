b2390a4d6b9ec277bcdb2c4fef07abfc7ed0397d
zakibourmada <119577305+zakibourmada@users.noreply.github.com>
2023-08-23 17:01:15 +0200

    new file created

---
+++ python_gui/gui_components/gui_helpers.py
@@ -0,0 +1,7 @@
+# python_gui/gui_components/gui_helpers.py
+
+def display_message(message):
+    print(f"Displaying message: {message}")
+
+def clear_screen():
+    print("\033[H\033[J")  # Clear terminal screen
